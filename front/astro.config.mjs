aba2c713e7a7089d416473f32220734b1eef50fc
a23brijaemed <a23brijaemed@inspedralbes.cat>
2025-03-04 17:04:19 +0100

    fix: problmes with docker-compose.yml fixed

--- front/astro.config.mjs
+++ front/astro.config.mjs
@@ -1,12 +1,20 @@
 import { defineConfig } from 'astro/config'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
   server: {
-    host: true,
-    port: import.meta.env.ASTRO_PORT || 4400
-  }
+    // watch: {
+    //   usePolling: true, // Habilita la recarga en vivo si tienes problemas con el sistema de archivos
+    // },
+    port: import.meta.env.PUBLIC_ASTRO_PORT || 4321
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve("./src"),  // ✅ Alias para la carpeta src
+    },
+  },
 })
