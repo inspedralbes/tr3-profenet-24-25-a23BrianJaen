e15c6b4825dc38966ce43336379112b1b5b0a756
a23brijaemed <a23brijaemed@inspedralbes.cat>
2025-03-17 17:19:11 +0100

    feat: added counter to selected courses and change style

--- frontNext/src/components/common/Modals/ModalCloneTeacher.tsx
+++ frontNext/src/components/common/Modals/ModalCloneTeacher.tsx
@@ -33,10 +33,10 @@ export default function ModalCloneTeacher({ isOpen, onClose, title, teacher, cou
     selectedClasses: selectedClasses,
   });
 
-  // Reset cuando se abre o cierra el modal
+  // Reset when the modal is open or closed
   useEffect(() => {
     if (isOpen) {
-      // Cuando se abre el modal
+      // When modal is open
       setAllClasses(courses);
       setSelectedClasses(courses);
       setStep(1);
@@ -143,97 +143,99 @@ export default function ModalCloneTeacher({ isOpen, onClose, title, teacher, cou
 
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black/50 backdrop-blur-sm z-50">
-      <div className={`p-6 rounded-lg shadow-lg max-w-xl min-h-96 w-full ${theme === "dark" ? "bg-[#0f1214]" : "bg-white"}`}>
+      <div className={`p-6 rounded-lg shadow-lg max-w-xl min-h-[32rem] w-full flex flex-col ${theme === "dark" ? "bg-[#0f1214]" : "bg-white"}`}>
         {/* Header */}
         <div className="flex items-center justify-between mb-4">
           <h2 className="text-xl font-semibold text-primary p-2">
             {title || "Clonar professor"}
           </h2>
-          <button className="px-4 py-2" >
+          <button className="px-4 py-2">
             <X className="h-7 w-7 cursor-pointer text-primary" onClick={handleCloseModal} />
           </button>
         </div>
 
-        {/* Steps */}
-        {step === 1 && (
-          <div>
-            <ModalTitle normalText="Selecciona un docent" highlightedText="origen" additionalText="per clonar" />
-            <SelectedCloneTeacher
-              key={teacher.id}
-              teacher={teacher}
-              selectedTeacher={selectedTeacherOrigin}
-              handleTeacherClick={() => handleTeacherClick(teacher.id, teacher.firstname, teacher.lastname)}
-            />
-          </div>
-        )}
-
-        {step === 2 && (
-          <div>
-            <ModalTitle normalText="Selecciona les assignatures per clonar" />
-            <SelectedCloneClasses
-              classes={allClasses}
-              selectedClasses={selectedClasses}
-              handleClassesClick={(classe) => handleClassesClick(classe.id, classe.name, classe.shortname)}
-              fetchMoreClasses={fetchMoreClasses}
-            />
-          </div>
-        )}
-
-        {step === 3 && (
-          <div>
-            <ModalTitle normalText="Selecciona un docent" highlightedText="destí" additionalText="per clonar" />
-            {/* Placeholder for a destination teacher - replace with real data */}
-            <SelectedCloneTeacher
-              key={"1"}
-              teacher={{ id: "1", firstname: "luis", lastname: "rodriguez" }}
-              selectedTeacher={selectedTeacherDestination}
-              handleTeacherClick={() => handleTeacherDestinationClick("1", "luis", "rodriguez")}
-            />
-          </div>
-        )}
-
-        {step === 4 && (
-          <div>
-            <ModalTitle normalText="Selecció total de les dades del docent" />
-            <ConfirmClone payload={payload} />
-          </div>
-        )}
-
-        {/* Footer - Navigation Buttons */}
-        <div className="mt-6 flex">
-          {/* Button previous */}
-          {step > 1 && (
-            <button
-              className="cursor-pointer p-2 bg-blue-500 text-white rounded-lg hover:bg-blue-700"
-              onClick={handlePreviousStep}
-            >
-              Enrera
-            </button>
+        {/* Content - Wrap steps in a div with flex-grow */}
+        <div className="relative h-full min-h-[400px] pb-[60px]">
+          {/* Steps content here */}
+          {step === 1 && (
+            <div>
+              <ModalTitle normalText="Selecciona un docent" highlightedText="origen" additionalText="per clonar" />
+              <SelectedCloneTeacher
+                key={teacher.id}
+                teacher={teacher}
+                selectedTeacher={selectedTeacherOrigin}
+                handleTeacherClick={() => handleTeacherClick(teacher.id, teacher.firstname, teacher.lastname)}
+              />
+            </div>
           )}
 
-          {/* Button next or "Clone Classes" in the last step*/}
-          <div className="ml-auto">
-            {step < 4 ? (
+          {step === 2 && (
+            <div>
+              <ModalTitle normalText="Selecciona les assignatures per clonar" classes={allClasses} selectedClasses={selectedClasses} />
+              <SelectedCloneClasses
+                classes={allClasses}
+                selectedClasses={selectedClasses}
+                handleClassesClick={(classe) => handleClassesClick(classe.id, classe.name, classe.shortname)}
+                fetchMoreClasses={fetchMoreClasses}
+              />
+            </div>
+          )}
+
+          {step === 3 && (
+            <div>
+              <ModalTitle normalText="Selecciona un docent" highlightedText="destí" additionalText="per clonar" />
+              {/* Placeholder for a destination teacher*/}
+              <SelectedCloneTeacher
+                key={"1"}
+                teacher={{ id: "1", firstname: "luis", lastname: "rodriguez" }}
+                selectedTeacher={selectedTeacherDestination}
+                handleTeacherClick={() => handleTeacherDestinationClick("1", "luis", "rodriguez")}
+              />
+            </div>
+          )}
+
+          {step === 4 && (
+            <div>
+              <ModalTitle normalText="Selecció total de les dades del docent" />
+              <ConfirmClone payload={payload} />
+            </div>
+          )}
+
+          {/* Footer - Navigation Buttons */}
+          <div className="absolute bottom-0 left-0 right-0 pt-6 flex">
+            {/* Button previous */}
+            {step > 1 && (
               <button
-                className="cursor-pointer p-2 bg-blue-500 text-white rounded-lg hover:bg-blue-700 justify-end"
-                onClick={handleNextStep}
-                disabled={
-                  (step === 1 && !selectedTeacherOrigin) ||
-                  (step === 2 && selectedClasses.length === 0) ||
-                  (step === 3 && !selectedTeacherDestination)
-                }
+                className="cursor-pointer p-2 bg-blue-500 text-white rounded-lg hover:bg-blue-700"
+                onClick={handlePreviousStep}
               >
-                Següent
-              </button>
-            ) : (
-              <button
-                className="cursor-pointer p-2 bg-green-500 text-white rounded-lg hover:bg-green-700"
-                onClick={handleSendRequestToBack}
-                disabled={!selectedTeacherDestination}
-              >
-                Clonar Clases
+                Enrera
               </button>
             )}
+
+            <div className="ml-auto">
+              {step < 4 ? (
+                <button
+                  className="cursor-pointer p-2 bg-blue-500 text-white rounded-lg hover:bg-blue-700 justify-end"
+                  onClick={handleNextStep}
+                  disabled={
+                    (step === 1 && !selectedTeacherOrigin) ||
+                    (step === 2 && selectedClasses.length === 0) ||
+                    (step === 3 && !selectedTeacherDestination)
+                  }
+                >
+                  Següent
+                </button>
+              ) : (
+                <button
+                  className="cursor-pointer p-2 bg-green-500 text-white rounded-lg hover:bg-green-700"
+                  onClick={handleSendRequestToBack}
+                  disabled={!selectedTeacherDestination}
+                >
+                  Clonar Clases
+                </button>
+              )}
+            </div>
           </div>
         </div>
       </div>
--- frontNext/src/components/common/Modals/ModalTitle.tsx
+++ frontNext/src/components/common/Modals/ModalTitle.tsx
@@ -1,15 +1,19 @@
 "use client"
 
+import { Courses } from "@/src/types/types";
+
 interface ModalTitleProps {
   normalText: string;
   highlightedText?: string;
   additionalText?: string;
+  classes?: Courses[];
+  selectedClasses?: Courses[];
 }
 
-export default function ModalTitle({ normalText, highlightedText, additionalText }: ModalTitleProps) {
+export default function ModalTitle({ normalText, highlightedText, additionalText, classes, selectedClasses }: ModalTitleProps) {
   return (
     <h2 className="text-primary text-center text-xl font-bold mb-1.5">
-      {normalText} <strong className="font-bold">{highlightedText}</strong> {additionalText}
+      {normalText} <strong className="font-bold">{highlightedText}</strong> {additionalText} {classes && selectedClasses && `(${selectedClasses.length}/${classes.length})`}
     </h2>
   );
 }
