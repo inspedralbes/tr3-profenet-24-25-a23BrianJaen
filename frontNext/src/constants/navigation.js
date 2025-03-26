af40a2a33b3c50241e806d221b121aa469cd1332
a23brijaemed <a23brijaemed@inspedralbes.cat>
2025-03-26 18:39:22 +0100

    fix: correct typo in navigation menu item
    Fix the spelling of "Porfessors" to "Profesors" in the navigation menu to ensure accuracy and professionalism in the UI

--- frontNext/src/constants/navigation.js
+++ frontNext/src/constants/navigation.js
@@ -2,7 +2,7 @@ import { LayoutDashboard, User } from "lucide-react";
 
 export const navigation = [
   { name: "Inici", href: "/", icon: LayoutDashboard },
-  { name: "Porfessors", href: "/teachers", icon: User, isDinamic: true },
+  { name: "Profesors", href: "/teachers", icon: User, isDinamic: true },
   // { name: "Els meus cursos", href: "/courses", icon: BookOpen },
   // { name: "Horari", href: "/schedule", icon: Calendar },
   // { name: "Configuració", href: "/settings", icon: Settings },
