5c472c2cad5bcd4db7a8e10a8cee716180a04e27
a23brijaemed <a23brijaemed@inspedralbes.cat>
2025-03-27 11:03:50 +0100

    chore: add staticPageGenerationTimeout to next.config.ts
    Set staticPageGenerationTimeout to 0 to prevent timeouts during static page generation

--- frontNext/next.config.ts
+++ frontNext/next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: false,
+  staticPageGenerationTimeout: 0,
 };
 
 export default nextConfig;
